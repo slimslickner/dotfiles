7a1e5eeaf17dac29cdc3ca27a92aafc6ffd8aefe
tim <41093512+slimslickner@users.noreply.github.com>
2026-04-05 14:53:56 -0400

    automatically open tree in nvim

--- .config/nvim/lua/config/plugins/nvim-tree.lua
+++ .config/nvim/lua/config/plugins/nvim-tree.lua
@@ -5,3 +5,21 @@ return {
     require("nvim-tree").setup()
   end,
 }
+
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("nvim-tree").setup()
+
+    -- auto-open when nvim is opened with a directory
+    local api = require("nvim-tree.api")
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function(data)
+        if vim.fn.isdirectory(data.file) == 1 then
+          api.tree.open()
+        end
+      end,
+    })
+  end,
+}
